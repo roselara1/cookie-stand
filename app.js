a18501008fa220d462be345bc28dc2e6534e5bdf
Rosie Lara <roselara1@gmail.com>
2018-01-27 23:46:30 -0800

    framework for lab7 started

--- app.js
+++ app.js
@@ -243,7 +243,7 @@ var alki = {
     this.calcCookieSoldPerHour();
     var alki = document.getElementById('alki');
     var al = document.getElementById('al');
-    //calling the methos in the object literal
+    //calling the methods in the object literal
 
 
     var h6El = document.createElement('h6');
@@ -263,3 +263,69 @@ var alki = {
 };
 alki.render();
 
+var allLocations = [];
+
+//define var that are dynamic depending on cookies
+var totalCookiesPerHour = 0;
+//total for all locations by hour
+
+var netTotal = 0;
+
+function MakeLocation(
+
+    name,
+    minCustPerHour,
+    maxCustPerHour,
+    avgCookieSoldPerHour
+)   {
+
+    this.name = name;
+    this.minCustPerHour = minCustPerHour;
+    this.maxCustPerHour = maxCustPerHour;
+    this.avgCookieSoldPerHour = avgCookieSoldPerHour;
+    this.randCustPerHour = [];
+    this.totalCookiesPerHour = [];
+    this.totalCookies = 0;
+    allocations.push(this);
+
+    //method for random customers by hour
+  calcRandCustPerHour: function() {
+    for (var a = 0; a < hours.length; a++) {
+      this.randCustPerHour.push(Math.floor(Math.random() * (this.maxCustPerhour - this.minCustPerHour + 1)), + this.minCustPerHour);
+      console.log(this.randCustPerHour[a]);
+    }
+  };
+
+  //method for cookies sold by hour
+  calcCookieSoldPerHour: function() {
+    for (var b = 0; b < hours.length; b++) {
+      this.cookieSoldPerHour.push(Math.random() * Math.round(this.avgcookieSoldPerHour * this.randCustPerHour[b]));
+      this.totalCookies += this.cookieSoldPerHour[b];
+      console.log(this.cookieSoldPerHour[b]);
+    }
+  };
+}
+
+function makeStands () {
+
+    new MakeLocation("First and Pike", 23, 65, 6.3);
+    new MakeLocation("SeaTac Airport", 3, 24, 1.2)
+    new MakeLocation("Seattle Center", 11, 38, 3.7);
+    new MakeLocation("Capitol Hill", 20, 38, 2.3);
+    new MakeLocation("Alki", 2, 16, 4.6);
+}
+makeStands();
+
+//create the table in js
+
+
+
+//make header row
+function makeHeaderRow() {
+    var cookiestands = document.getElementById('cookiestands');
+    var trEL = document.createElement("tr");
+    var thEl = document.createElement("th");
+    trEl.appendChild(thEl);
+}
+
+//table needs an id in html
